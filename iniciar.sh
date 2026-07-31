b04fd28f3d14747b38023cd8fdde628f361c52b5
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2026-07-31 19:58:07 -0300

    feat: modificaciones script de inicio

--- iniciar.sh
+++ iniciar.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 cp -r data ../
+# por ahora no lo cambiamos a pnpm
 npm install 
 # corremos las migraciones 
 cd src/database
 npx sequelize-cli db:migrate
+# creamos una clave para jwt
+clave=$(echo -n "algo" | md5sum | awk '{print $1}')
+cd ../..
+echo "key=$clave" > .env
